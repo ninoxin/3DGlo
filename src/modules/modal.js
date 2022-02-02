dadd09d986beaeaabdaac5143802cc7e2d0f13b6
ninoxin <ninoxin2003@mail.ru>
2022-02-02 10:59:02 +0300

    переделали модалку

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,7 +1,6 @@
 const modal = () => {
     const modal = document.querySelector('.popup')
     const buttons = document.querySelectorAll('.popup-btn')
-    const closeBtn = modal.querySelector('.popup-close')
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
@@ -12,7 +11,6 @@ const modal = () => {
     closeBtn.addEventListener('click', () => {
         modal.style.display = 'none'
     })
-
     modal.addEventListener('click', (e) => {
         if (
             !e.target.closest(".popup-content") ||
